c9fad958f96236761c3786e38787ac2c10fa2cb4
Juan Ramirez <juanramirez@mail.usf.edu>
2017-01-21 19:04:12 -0500

    Works and looks fine. Added button to show another view.

--- Miracle Pill/ViewController.swift
+++ Miracle Pill/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var buyNowOutlet: UIButton!
+    
+    @IBOutlet weak var stateButtonText: UIButton!
+    
+    let states = ["Alabama", "California", "Florida", "New York","Texas"]
+    
+    @IBAction func showStatePicker(_ sender: Any) {
+        statePicker.isHidden = false
+        
+    }
+    
+    @IBAction func buyNowButton(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // This is like the number of columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateButtonText.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 
 }
 
